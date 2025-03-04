28f6d4cb3cf2190fa65e07c0fa75389ee9905961
harshlsoni <harshlsoni2920@gmail.com>
2025-03-04 22:05:24 +0530

    StudentManager.java

---
+++ StudentManager.java
@@ -0,0 +1,10 @@
+// StudentManager.java
+import java.util.*;
+
+public class StudentManager {
+    private List<Student> students;  // ArrayList to store Student objects
+
+    public StudentManager() {
+        students = new ArrayList<>();
+    }
+}
